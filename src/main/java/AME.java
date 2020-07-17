6eacf4a73782fa31b6eb5fa285f1fd3500df3fc8
Vestri <p.prokop@firenja.com>
2020-07-17 14:50:42 +0200

    init

---
+++ src/main/java/AME.java
@@ -0,0 +1,58 @@
+package main.java;
+
+import main.java.config.ConfigManager;
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AME extends JavaPlugin
+{
+    static AME instance;
+
+    public void onEnable()
+    {
+        instance = this;
+        new ConfigManager();
+        try
+        {
+            ConfigManager.loadConfig();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        Bukkit.getLogger().info("[AME] register commands!");
+        regCommands();
+        Bukkit.getLogger().info("[AME] commands registered!");
+        Bukkit.getLogger().info("[AME] load events!");
+
+        Bukkit.getLogger().info("[AME] events loaded!");
+        regEvents();
+        Bukkit.getLogger().info("[AME] has been enabled!");
+    }
+
+    private void regCommands()
+    {
+     //   this.getCommand("command").setExecutor(new Command());
+
+    }
+
+    private void regEvents()
+    {
+        PluginManager pm = getServer().getPluginManager();
+       // pm.registerEvents(new OnEntityDeath(), this);
+    }
+
+    @Override
+    public void onDisable()
+    {
+        HandlerList.unregisterAll(this);
+        Bukkit.getLogger().info("[AME] has been disabled!");
+    }
+
+    public static AME getInstance()
+    {
+        return instance;
+    }
+}
---
+++ src/main/java/config/ConfigManager.java
@@ -0,0 +1,41 @@
+package main.java.config;
+
+import main.java.AME;
+
+import java.io.File;
+
+public class ConfigManager
+{
+
+    static ConfigManager instance;
+
+    public ConfigManager()
+    {
+        instance = this;
+    }
+
+    public static ConfigManager getInstance()
+    {
+        return instance;
+    }
+
+    public static void loadConfig()
+    {
+        File configFile = new File("plugins" + File.separator + AME.getInstance().getName() + File.separator + "config.yml");
+        if (!configFile.exists())
+        {
+            AME.getInstance().getLogger().info("Creating config ...");
+            AME.getInstance().saveDefaultConfig();
+        }
+        try
+        {
+            AME.getInstance().getLogger().info("Loading the config ...");
+            AME.getInstance().getConfig().load(configFile);
+        }
+        catch (Exception e)
+        {
+            AME.getInstance().getLogger().severe("Could not load the config! Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
